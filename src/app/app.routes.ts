157f88edf5bb9aa6b6a2ecb3f70a284c60e712bb
m-mariam <mariamabdelaziz622@gmail.com>
2025-07-25 17:13:35 +0300

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { CardComponent } from './Components/card/card.component';
+import { NotfoundComponent } from './Components/notfound/notfound.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'Home', component: HomeComponent},
+    {path:'card', component: CardComponent},
+    {path: '**', component: NotfoundComponent}
+
+];
