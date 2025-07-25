147933b5b27f1447fcf168f0785519a0952d7899
m-mariam <mariamabdelaziz622@gmail.com>
2025-07-25 22:28:11 +0300

    added product service and displayed products and product details and added navbar

--- src/app/Services/product.service.ts
+++ src/app/Services/product.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
-
-  constructor() { }
+  constructor(private _HttpClient: HttpClient) { }
+  getAllProducts = ():Observable<any> => {
+    return this._HttpClient.get( `https://fakestoreapi.com/products`)}
+    
+  getProduct = (id:string):Observable<any> => {
+      return this._HttpClient.get( `https://fakestoreapi.com/products/${id}`) }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './Components/home/home.component';
 import { CardComponent } from './Components/card/card.component';
 import { NotfoundComponent } from './Components/notfound/notfound.component';
+import { ProductDetailsComponent } from './Components/product-details/product-details.component';
 
 export const routes: Routes = [
-    {path:'Home', component: HomeComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+    {path:'home', component: HomeComponent},
+    { path: 'products/:id', component: ProductDetailsComponent },
     {path:'card', component: CardComponent},
     {path: '**', component: NotfoundComponent}
 
