1cc22b8ea2ea1315637ff0858b0354d20973eb0f
m-mariam <mariamabdelaziz622@gmail.com>
2025-07-26 15:38:39 +0300

    added footer and cart service and notfound page

--- src/app/Components/navbar/navbar.component.ts
+++ src/app/Components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartService } from '../../Services/cart.service';
 
 @Component({
   selector: 'app-navbar',
@@ -9,7 +10,16 @@ import { RouterLink } from '@angular/router';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
 cartCount = 0;
-
-
+constructor(private cartService: CartService){}
+ngOnInit(): void {
+  this.cartService.cart.subscribe(items => {
+    this.cartCount = items.length;
+  });
+}
+toggleDarkMode() {
+  const htmlEl = document.documentElement;
+  htmlEl.classList.toggle('dark');
+}
 }
--- src/app/Services/cart.service.ts
+++ src/app/Services/cart.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 
-  constructor() { }
+private cartItems: any[] = [];
+  private cartSubject = new BehaviorSubject<any[]>([]);
+
+  cart = this.cartSubject.asObservable();
+
+  constructor() {}
+
+  addToCart(product: any) {
+    this.cartItems.push(product);
+    this.cartSubject.next(this.cartItems); // Update subscribers
+  }
+
+  getCartItems() {
+    return this.cartItems;
+  }
+
+  clearCart() {
+    this.cartItems = [];
+    this.cartSubject.next([]);
+  }
+
+  getCartCount() {
+    return this.cartItems.length;
+  }
+removeFromCart(index: number) {
+  this.cartItems.splice(index, 1);
+  this.cartSubject.next(this.cartItems);
+}
+
 }
